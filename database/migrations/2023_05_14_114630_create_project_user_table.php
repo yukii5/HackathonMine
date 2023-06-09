2cdacebc03d362d41998cad114616f72a141e41c
kenchang198 <sasamotoken@gmail.com>
2023-06-09 21:31:37 +0900

    プロジェクト- メンバー 中間テーブル定義変更

--- database/migrations/2023_05_14_114630_create_project_user_table.php
+++ database/migrations/2023_05_14_114630_create_project_user_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProjectMembersTable extends Migration
+class CreateProjectUserTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateProjectMembersTable extends Migration
      */
     public function up()
     {
-        Schema::create('project_members', function (Blueprint $table) {
+        Schema::create('project_user', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('project_id');
             $table->foreign('project_id')->references('id')->on('projects');
@@ -29,6 +29,6 @@ class CreateProjectMembersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('project_members');
+        Schema::dropIfExists('project_user');
     }
 }
