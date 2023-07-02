82ad662e4ae19a0907863427f4045a039628bd1d
kenchang198 <sasamotoken@gmail.com>
2023-07-02 20:05:01 +0900

    Comments 定義修正

--- database/migrations/2023_05_14_114658_create_comments_table.php
+++ database/migrations/2023_05_14_114658_create_comments_table.php
@@ -15,8 +15,8 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('project_id');
-            $table->foreign('project_id')->references('id')->on('projects');
+            $table->unsignedBigInteger('ticket_id');
+            $table->foreign('ticket_id')->references('id')->on('tickets');
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users');
             $table->text('comment');
