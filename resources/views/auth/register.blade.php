35da3e39f4b82fd47b574444b36c521a231af7c4
kenchang198 <sasamotoken@gmail.com>
2023-07-24 09:23:17 +0900

    ユーザ登録画面 作成準備

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,77 +1,64 @@
-@extends('layouts.app')
+<!doctype html>
+<html lang="ja">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Register') }}</div>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Hackathon</title>
+</head>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('register') }}">
-                        @csrf
+<body>
+    <header class="navbar text-light bg-dark">
+        <div class="container-fluid">
+            <b class="fs-4">Hackathon Mine</b>
+        </div>
+    </header>
+    <div class="container-fluid">
+        <nav class="my-4" style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="/">TOP</a></li>
+                <li class="breadcrumb-item"><a href="/users.html">ユーザー一覧</a></li>
+                <li class="breadcrumb-item active" aria-current="page">新規登録</li>
+            </ol>
+        </nav>
+        <form enctype="multipart/form-data" class="mt-5 entry-form" action="/" method="post">
+            
+            <div class="mb-4">
+                <label for="name" class="form-label">ユーザー名</label>
+                <input type="text" name="name" class="form-control">
+            </div>
+            <div class="mb-4">
+                <label for="email" class="form-label">メールアドレス</label>
+                <input type="email" name="email" class="form-control">
+            </div>
+            <div class="mb-4">
+                <label for="password" class="form-label">パスワード</label>
+                <input type="password" name="password" class="form-control">
+            </div>
+            <div class="mb-4">
+                <label for="password_confirmation" class="form-label">確認用パスワード</label>
+                <input type="password_confirmation" name="password_confirmation" class="form-control">
+            </div>
+            <div class="mb-4">
+                <label for="admin" class="form-label">権限</label>
+                <div class="form-check">
+                    <input id="nomal_user" class="form-check-input" type="radio" name="admin" value="0" checked>
+                    <label class="form-check-label" for="nomal_user">通常</label>
+                </div>
+                <div class="form-check">
+                    <input id="admin_user" class="form-check-input" type="radio" name="admin" value="1">
+                    <label class="form-check-label" for="admin_user">管理者</label>
+                </div>
 
-                        <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                                @error('name')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email">
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="new-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="row mb-3">
-                            <label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Confirm Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Register') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+                <div class="d-flex justify-content-center complete-btn-grp pt-5 mb-5">
+                    <div><a class="text-light btn btn-secondary me-3" href="/"><b>戻る</b></a></div>
+                    <button type="submit" class="btn btn-primary me-3"><b>登録</b></button>
                 </div>
             </div>
-        </div>
+        </form>
     </div>
-</div>
-@endsection
+</body>
+
+</html>
