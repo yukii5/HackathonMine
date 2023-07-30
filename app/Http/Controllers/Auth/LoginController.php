24855ea52c288bc9fee7bb8a8b56d02b8e6ff081
kenchang198 <sasamotoken@gmail.com>
2023-07-30 14:39:51 +0900

    論理削除したユーザをログイン不可にする

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -42,5 +43,64 @@ class LoginController extends Controller
     protected function loggedOut(Request $request)
     {
         return redirect(route('login'));
+    }
+
+    /**
+     * Copy from vendor/laravel/ui/auth-backend/AuthenticatesUsers.php
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Http\Response|\Illuminate\Http\JsonResponse
+     *
+     * @throws \Illuminate\Validation\ValidationException
+     */
+    public function login(Request $request)
+    {
+        $this->validateLogin($request);
+
+        if (method_exists($this, 'hasTooManyLoginAttempts') &&
+            $this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
+        if ($this->attemptLogin($request)) {
+
+            if ($request->hasSession()) {
+                $request->session()->put('auth.password_confirmed_at', time());
+            }
+
+            return $this->sendLoginResponse($request);
+        }
+
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
+    }
+
+    /**
+     * Copy from vendor/laravel/ui/auth-backend/AuthenticatesUsers.php
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     */
+    protected function attemptLogin(Request $request)
+    {
+        // return $this->guard()->attempt(
+        //     $this->credentials($request), $request->boolean('remember')
+        // );
+        $result = $this->guard()->attempt(
+            $this->credentials($request), $request->boolean('remember')
+        );
+        
+        $user = Auth::user();
+        
+        if ($user && $user->del_flg) {
+            $this->guard()->logout();
+            $request->session()->invalidate();
+            return false;
+        }
+
+        return $result;
     }
 }
